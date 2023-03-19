e66c12192b7baffdaf5998428fdfc11eae6102d7
steve-chude <125799928+steve-chude@users.noreply.github.com>
2023-03-19 15:56:33 +0100

    helper.js file

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,12 +1,26 @@
-const baseURL = "http://localhost:3000/";
+const baseURL = "http://localhost:3000/api/posts";
 
+// endpoint: http://localhost:3000/api/posts
 export default async function getPost(id) {
-  const endpoint = id ? `api/posts/${id}` : "api/posts";
-  const res = await fetch(`${baseURL}${endpoint}`);
-  const data = await res.json();
+  const res = await fetch(`${baseURL}`);
+  const posts = await res.json();
 
-  return data;
+  if (id) {
+    return posts.find((value) => value.id == id);
+  }
+
+  return posts;
 }
+
+// const baseURL = "http://localhost:3000/";
+
+// export default async function getPost(id) {
+//   const endpoint = id ? `api/posts/${id}` : "api/posts";
+//   const res = await fetch(`${baseURL}${endpoint}`);
+//   const data = await res.json();
+
+//   return data;
+// }
 
 // endpoint: http://localhost:3000/api/posts
 
